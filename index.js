38f49fe091cfd81d3e855b89e28b3e2b0c26a347
Kimmo T <tuokkolk@gmail.com>
2024-05-11 23:19:16 +0300

    Tietojen tallettaminen MongoDB-tietokantaan - tehtävät 3.15 - 3.18

--- index.js
+++ index.js
@@ -4,30 +4,6 @@ const http = require('http')
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const Person = require('./models/person')
-  /**
-   * Middleware on funktio, joka saa kolme parametria.
-   * Middleware kutsuu lopussa parametrina olevaa funktiota next,
-   * jolla se siirtää kontrollin seuraavalle middlewarelle.
-   * 
-   * Middlewaret suoritetaan siinä järjestyksessä, jossa ne on otettu käyttöön
-   * sovellusolion metodilla use. Huomaa, että json-parseri tulee ottaa käyttöön 
-   * ennen middlewarea requestLogger, muuten request.body ei ole vielä alustettu 
-   * loggeria suoritettaessa
-   * @param {*} request 
-   * @param {*} response 
-   * @param {*} next 
-   */
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
 
 morgan.token('body', request => {
   return Object.keys(request.body).length === 0 ? null : JSON.stringify(request.body)
@@ -43,6 +19,27 @@ app.use(cors())
 app.use(express.json())
 app.use(bodyParser.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+/**
+ * Middleware on funktio, joka saa kolme parametria.
+ * Middleware kutsuu lopussa parametrina olevaa funktiota next,
+ * jolla se siirtää kontrollin seuraavalle middlewarelle.
+ * 
+ * Middlewaret suoritetaan siinä järjestyksessä, jossa ne on otettu käyttöön
+ * sovellusolion metodilla use. Huomaa, että json-parseri tulee ottaa käyttöön 
+ * ennen middlewarea requestLogger, muuten request.body ei ole vielä alustettu 
+ * loggeria suoritettaessa
+ * @param {*} request 
+ * @param {*} response 
+ * @param {*} next 
+ */
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
 //app.use(requestLogger)
 
 const validatePerson = async (person, isPush) => {
@@ -75,7 +72,7 @@ const validatePerson = async (person, isPush) => {
 app.get('/api/persons', (request, response) => {
   Person.find({})
     .then(persons => response.json(persons))
-    .catch(error =>response.status(400).json({ error }))
+    .catch(error =>response.status(500).json({ error }))
 })
 
 /**
@@ -97,7 +94,7 @@ app.get('/info', (request, response) => {
 /**
  * Get person by id
  */
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id).then(person => {
     console.log(`Get person: id=${id}, data=${person}`)
     if (person) {
@@ -107,9 +104,12 @@ app.get('/api/persons/:id', (request, response) => {
       response.status(404).end()
     }
   })
-  .catch(error =>
-    response.status(400).json({ error }))
-
+  .catch(error => {
+    console.log(error)
+    // Kun next funktion kutsussa annetaan parametri,
+    // siirtyy käsittely virheidenkäsittelymiddlewarelle
+    next(error)
+  })
 })
 
 /**
@@ -118,8 +118,10 @@ app.get('/api/persons/:id', (request, response) => {
 app.delete('/api/persons/:id', (request, response) => {
   Person.findByIdAndDelete(request.params.id)
     .then(result => response.status(204).end())
-    .catch(error =>
-      response.status(400).json({ error }))
+    .catch(error => {
+      //response.status(400).json({ error })
+      error => next(error)
+    })
 
 })
 
@@ -142,8 +144,10 @@ app.post('/api/persons', async (request, response) => {
     .then(savedPerson =>
       response.json(savedPerson)
     )
-    .catch(error =>
-      response.status(400).json({ error }))
+    .catch(error => {
+      //response.status(400).json({ error })
+      error => next(error)
+    })
 })
 
 /**
@@ -166,9 +170,14 @@ app.put('/api/persons/:id', async (request, response) => {
     })
     .catch(error => {
       console.log('Update error', error)
-      response.status(400).json({ error })
+      //response.status(400).json({ error })
+      error => next(error)
     })
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
 /**
  * Middlewaret tulee ottaa käyttöön ennen routeja, jos ne halutaan suorittaa
@@ -177,6 +186,19 @@ app.put('/api/persons/:id', async (request, response) => {
  * suoritetaan vain, jos mikään route ei käsittele HTTP-pyyntöä.
  */
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  next(error)
+}
+
+// Virheenkäsittely rekisteröidään kaikkien muiden middlewarejen ja routejen
+// rekisteröinnin jälkeen!
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
